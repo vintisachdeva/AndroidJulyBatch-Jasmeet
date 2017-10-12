e5f7c1cfefa0b21a461f93c94eb58e49c124977a
vintisachdeva <vintisachdeva17@gmail.com>
2017-10-12 11:25:04 +0530

    Add files via upload

---
+++ 11 Oct- PurblePlace/purbleplace/CustomAdapter.kt
@@ -0,0 +1,28 @@
+package com.bmpl.purbleplace
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class CustomAdapter : BaseAdapter(){//abstract class
+
+    var images  = intArrayOf()
+
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItem(p0: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(p0: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
---
+++ 11 Oct- PurblePlace/purbleplace/GameActivity.kt
@@ -0,0 +1,15 @@
+package com.bmpl.purbleplace
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+        //4 X 4 = 16 images -> 8 images
+
+    }
+}
